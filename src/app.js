a95194e2594bffdcdf632c01c089d429913f8e66
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-05-11 03:47:30 +0900

    enables to customize color of roads

--- src/app.js
+++ src/app.js
@@ -11,16 +11,33 @@ map.on('load', () => {
 
   // Place color picker
   AColorPicker.from( '#color-picker' ).on( 'change', ( picker, color ) => {
-    const rgb = AColorPicker.parseColor( color, "rgb" );
-    const waterColor = rgb.map( value => value * 0.5 )
-    map.setPaintProperty( 'background', 'background-color', color );
-    map.setPaintProperty( 'water', 'fill-color', AColorPicker.parseColor( waterColor, "rgbcss" ) );
-    map.setPaintProperty( 'waterway_tunnel', 'line-color', AColorPicker.parseColor( waterColor, "rgbcss" ) );
-    map.setPaintProperty( 'waterway', 'line-color', AColorPicker.parseColor( waterColor, "rgbcss" ) );
+    const select = document.querySelector('#select').value
+
+    if ( 'background' === select ) {
+      const rgb = AColorPicker.parseColor( color, "rgb" );
+      const waterColor = rgb.map( value => value * 0.5 )
+      map.setPaintProperty( 'background', 'background-color', color );
+      map.setPaintProperty( 'water', 'fill-color', AColorPicker.parseColor( waterColor, "rgbcss" ) );
+      map.setPaintProperty( 'waterway_tunnel', 'line-color', AColorPicker.parseColor( waterColor, "rgbcss" ) );
+      map.setPaintProperty( 'waterway', 'line-color', AColorPicker.parseColor( waterColor, "rgbcss" ) );
+    } else if ( 'road' === select ) {
+      map.setPaintProperty( 'aeroway-runway', 'line-color', color );
+      map.setPaintProperty( 'aeroway-taxiway', 'line-color', color );
+      map.setPaintProperty( 'highway-path', 'line-color', color );
+      map.setPaintProperty( 'highway-motorway-link', 'line-color', color );
+      map.setPaintProperty( 'highway-link', 'line-color', color );
+      map.setPaintProperty( 'highway-minor', 'line-color', color );
+      map.setPaintProperty( 'highway-secondary-tertiary', 'line-color', color );
+      map.setPaintProperty( 'highway-primary', 'line-color', color );
+      map.setPaintProperty( 'highway-trunk', 'line-color', color );
+      map.setPaintProperty( 'highway-motorway', 'line-color', color );
+      map.setPaintProperty( 'bridge-trunk-primary', 'line-color', color );
+      map.setPaintProperty( 'railway-hatching', 'line-color', color );
+    }
   } )
 
-  map.addControl(new ExportControl({
+  map.addControl( new ExportControl( {
     dpi: 300,
     attribution: "© Geolonia © OpenStreetMap Contributors",
-  }))
+  } ) )
 })
